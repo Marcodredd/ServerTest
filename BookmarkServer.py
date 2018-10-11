96b5cf2ee2598f8e93f019cf596de493f615b6ef
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-10-11 01:22:49 +0100

    seventh commit

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -44,7 +44,8 @@ def CheckURI(uri, timeout=5):
         # If the GET request raised an exception, it's not OK.
         return False
 
-
+class ThreadHTTPServer(ThreadMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
 class Shortener(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         # A GET request will either be for / (the root path) or for /some-name.
@@ -109,5 +110,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
